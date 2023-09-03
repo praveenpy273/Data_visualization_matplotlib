d505ed73017477f2dd351534388ec44244bc6253
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-09-03 23:41:35 +0100

    updated code to vcover large number of points(1000)

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 flg,ax = plt.subplots()
-ax.scatter(x_values,y_values,s=50)
+ax.scatter(x_values,y_values,s=10)
 # ax.plot(input_values,squares,linewidth= 2)
 
 #Set chart title and label axes.
@@ -15,5 +15,7 @@ ax.set_ylabel('Square of Value', fontsize = 14)
 
 # Set size of tick_labels
 ax.tick_params(axis = 'both', labelsize = 14)
-
+ax.axis([0,1100, 0,1100000]) 
+'''axis method is used to specify range of each axis, it takes 4 points, 
+2 for x axis and 2 for y-axisx-axis: 0,1100, y-axis 0, 1100000'''
 plt.show()
