fec9ea0f2c539f211151c577068aa6429d590324
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-09-13 22:35:34 +0100

    added cmap to show the order of points in the walk

--- rw_visual.py
+++ rw_visual.py
@@ -2,11 +2,21 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # Make a random walk
+
 rw = RandomWalk()
 rw.fill_walk()
 
 # Plot the points in the walk.
 plt.style.use('classic')
 fig,ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+
+# to color the points according to their numbers we will store them in a list
+point_numbers = range(rw.num_points)
+
+# we will assign the list to variable c and to remove the outline,
+# we will pass none to edge colors so that ponts are cleare.
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 plt.show()
+
+
+
