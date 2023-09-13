acf72f07ffa38afac1084fd422ceb3151adf6dda
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-09-13 22:49:30 +0100

    highlighted starting and end point

--- rw_visual.py
+++ rw_visual.py
@@ -16,6 +16,11 @@ point_numbers = range(rw.num_points)
 # we will assign the list to variable c and to remove the outline,
 # we will pass none to edge colors so that ponts are cleare.
 ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+# resizing first and last points.
+ax.scatter(rw.x_values[0], rw.y_values[0], c ='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
 plt.show()
 
 
