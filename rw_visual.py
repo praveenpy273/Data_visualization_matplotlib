1b622e542c952b0e82a07a634be02038f3601933
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-09-13 22:55:05 +0100

    removed axes so that they dint distract from the path of walk

--- rw_visual.py
+++ rw_visual.py
@@ -21,6 +21,10 @@ ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecol
 ax.scatter(rw.x_values[0], rw.y_values[0], c ='green', edgecolors='none', s=100)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
+# Removing the axes
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
 plt.show()
 
 
