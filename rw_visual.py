c5ec2c41b6d9bcddbda47f4d0083acbbcbe58a62
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-09-14 23:15:02 +0100

    Added figsize to adjust the size of plot to fill the screen

--- rw_visual.py
+++ rw_visual.py
@@ -8,14 +8,17 @@ rw.fill_walk()
 
 # Plot the points in the walk.
 plt.style.use('classic')
-fig,ax = plt.subplots()
+
+# figsize parameter takes tuple, which tells Matplotlib of the plotting window
+# using figsize so that plot fills the screen
+fig,ax = plt.subplots(figsize =(15,9))
 
 # to color the points according to their numbers we will store them in a list
 point_numbers = range(rw.num_points)
 
 # we will assign the list to variable c and to remove the outline,
 # we will pass none to edge colors so that ponts are cleare.
-ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 
 # resizing first and last points.
 ax.scatter(rw.x_values[0], rw.y_values[0], c ='green', edgecolors='none', s=100)
